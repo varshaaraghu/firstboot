ded1b53e463e9129f516c35f6e7f529a09ad5c1b
varshaaraghu <43814788+varshaaraghu@users.noreply.github.com>
2024-03-24 21:22:17 +0530

    Update Demo.java
    reverting changge

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Welcome to Simple Jenkin Job");
-         System.out.println("Welcome to Simple Jenkin Job sample")
+         System.out.println("Welcome to Simple Jenkin Job sample");
     }
 }
